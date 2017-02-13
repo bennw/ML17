603cd167c24f4efacfd51a136b79adc282c36674
bwn13 <bwn13@eews305-024.ee.ic.ac.uk>
2017-02-13 16:20:59 +0000

    trees_clean.mat is the decision tree deliverable (contains all 6 trees)

--- GenerateCleanTrees.m
+++ GenerateCleanTrees.m
@@ -2,7 +2,7 @@ function [ ] = GenerateCleanTrees( x,y )
     ain = 1:1:45;
     xin = x;
     for i=1:6
-        tree = DecisionTreeLearning(xin,ain,y==i);
-        DrawDecisionTree(tree, strcat('Clean data-trained tree for emotion ',num2str(i)));
+        tree(i) = DecisionTreeLearning(xin,ain,y==i);
+        DrawDecisionTree(tree(i), strcat('Clean data-trained tree for emotion ',num2str(i)));
     end
 end
